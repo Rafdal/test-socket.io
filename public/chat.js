483aaa1a95bbd2e3e78949451657f295aad9274b
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2020-07-19 11:48:50 -0300

    Primer commit

---
+++ public/chat.js
@@ -0,0 +1,34 @@
+console.log('chat codigo cliente');
+
+const socket = io(); // Solo funciona asi si lo despliego en el mismo dominio, caso contrario debo indicarlo: io('dominio')
+
+// DOM elements
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+btn.addEventListener('click', function(){
+    msg = {
+        username: username.value,
+        message: message.value
+    }
+    socket.emit('chat:mensaje', msg);
+    message.value = ''; // Borro el textBox del body mensaje
+});
+
+message.addEventListener('keypress', function() {
+    socket.emit('chat:escribiendo', username.value);
+});
+
+socket.on('chat:mensaje', function(data) { 
+    actions.innerHTML = ''
+    output.innerHTML += `<p>
+    <strong>${data.username}</strong>: ${data.message}
+    </p>`
+});
+
+socket.on('chat:escribiendo', function(data) {    
+    actions.innerHTML = `<p><em>${data} esta escribiendo</em></p>`
+});
